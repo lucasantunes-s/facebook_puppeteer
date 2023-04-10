2cb004a33ab4549866a5182a706d36ba8db872bb
Lucas Antunes <lucasssantunes@hotmail.com>
2023-04-10 09:14:33 -0300

    /teste

--- server.js
+++ server.js
@@ -2,38 +2,104 @@ const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const cheerio = require("cheerio");
 
 let search = "Jair Messias Bolsonaro";
 let url = `https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=BR&q=${search}&sort_data[direction]=desc&sort_data[mode]=relevancy_monthly_grouped&search_type=keyword_unordered&media_type=all`
 
-app.get("/", (req, res) => {
-    res.send("Hello Wolrd");
-})
-
-
-app.get("/face", async (req, res) => {
+// app.get("/", async (req, res) => {
+//     res.send("Hello Wolrd");
+    
+//     const browser = await puppeteer.launch({headless: false});
+//     const page = await browser.newPage();
+//     await page.goto(url);
+    
+//     const html = await page.content();
+//     const jsonOutput = htmlToJson(html);
   
+//     console.log(jsonOutput);
+//     await browser.close();
+    
+//     function htmlToJson(htmlContent) {
+//         const $ = cheerio.load(htmlContent);
+//         const result = {};
+//         // Coloque aqui a lógica de extração de informações do HTML e popule o objeto result.
+//         // Exemplo:
+//         result.title = $('title').text();
+//         result.headings = [];
+//         $('h1, h2, h3, h4, h5, h6').each(function (index, element) {
+//         result.headings.push({
+//             type: $(element).prop('tagName'),
+//             text: $(element).text()
+//     });
+//   });
+//   return result;
+// }
 
-  const puppeteer = require('puppeteer');
+// console.log(htmlToJson(html));
+    
+// })
 
-  
+
+app.get("/teste", async (req, res) => {
+
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto(url);
-
-    page.waitForSelector("text/usam esse criativo e esse texto");
-    const text = await page.$$eval("text/usam esse criativo e esse texto", element => element.map(element => element.innerHTML));
-    console.log(text);
-
-    ;
-   
+    const htmlContent = await page.content();
   
-    
-    
-    
- 
-  
+    const jsonOutput = htmlToJson(htmlContent);
+    console.log(jsonOutput);
+    await browser.close();
+
+
+    function htmlToJson(html) {
+    const $ = cheerio.load(html);
+    const result = {};
+    // Coloque aqui a lógica de extração de informações do HTML e popule o objeto result.
+    // Exemplo:
+    result.title = $('title').text();
+    result.headings = [];
+
+   $('h1, h2, h3, h4, h5, h6').each(function (index, element) {
+      result.headings.push({
+      type: $(element).prop('tagName'),
+      text: $(element).text()
+    });
+  });
+
+  return result;
+}
+
 })
+
+
+
+
+
+
+
+
+
+// app.get("/face", async (req, res) => {
+  
+
+//   const puppeteer = require('puppeteer');
+
+  
+//     const browser = await puppeteer.launch({headless: false});
+//     const page = await browser.newPage();
+//     await page.goto(url);
+
+//     page.waitForSelector("text/usam esse criativo e esse texto");
+//     const element = await page.$$eval("text/usam esse criativo e esse texto", element => element.map(element => element.innerHTML))
+//     console.log(element);
+
+//     element.forEach(element => console.log(element.textContent));;
+
+//     ;
+  
+// })
 
 app.listen(PORT, () => {
     console.log("App listen port " + PORT);
