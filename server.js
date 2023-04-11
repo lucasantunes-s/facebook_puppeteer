1289e3b761f31f9035b695a38fe41e0e0f925e45
Lucas Antunes <lucasssantunes@hotmail.com>
2023-04-11 10:12:21 -0300

    Parsing the response in a array.

--- server.js
+++ server.js
@@ -7,39 +7,6 @@ const cheerio = require("cheerio");
 let search = "Saca Rolha";
 let url = `https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=BR&q=${search}&sort_data[direction]=desc&sort_data[mode]=relevancy_monthly_grouped&search_type=keyword_unordered&media_type=all`
 
-// app.get("/", async (req, res) => {
-//     res.send("Hello Wolrd");
-    
-//     const browser = await puppeteer.launch({headless: false});
-//     const page = await browser.newPage();
-//     await page.goto(url);
-    
-//     const html = await page.content();
-//     const jsonOutput = htmlToJson(html);
-  
-//     console.log(jsonOutput);
-//     await browser.close();
-    
-//     function htmlToJson(htmlContent) {
-//         const $ = cheerio.load(htmlContent);
-//         const result = {};
-//         // Coloque aqui a lógica de extração de informações do HTML e popule o objeto result.
-//         // Exemplo:
-//         result.title = $('title').text();
-//         result.headings = [];
-//         $('h1, h2, h3, h4, h5, h6').each(function (index, element) {
-//         result.headings.push({
-//             type: $(element).prop('tagName'),
-//             text: $(element).text()
-//     });
-//   });
-//   return result;
-// }
-
-// console.log(htmlToJson(html));
-    
-// })
-
 
 app.get("/teste", async (req, res) => {
 
@@ -56,21 +23,14 @@ app.get("/teste", async (req, res) => {
     function htmlToJson(html) {
     const $ = cheerio.load(html);
     const result = {};
-    // Coloque aqui a lógica de extração de informações do HTML e popule o objeto result.
-    // Exemplo:
+    
     page.waitForSelector('.xdoe023');
     result.title = $('.xdoe023').text();
     result.headings = [];
     
+    result.response  = result.title.split("AtivoVeiculação");
 
-  //  $('._7jvw .x2izyaf .x1hq5gj4 .x1d52u69').each(function (index, element) {
-  //     result.headings.push({
-  //     type: $(element).prop('tagName'),
-  //     text: $(element).text()
-  //   });
-  // });
-
-  return result;
+  console.log(result.response);
 }
 
 })
@@ -78,30 +38,6 @@ app.get("/teste", async (req, res) => {
 
 
 
-
-
-
-
-
-// app.get("/face", async (req, res) => {
-  
-
-//   const puppeteer = require('puppeteer');
-
-  
-//     const browser = await puppeteer.launch({headless: false});
-//     const page = await browser.newPage();
-//     await page.goto(url);
-
-//     page.waitForSelector("text/usam esse criativo e esse texto");
-//     const element = await page.$$eval("text/usam esse criativo e esse texto", element => element.map(element => element.innerHTML))
-//     console.log(element);
-
-//     element.forEach(element => console.log(element.textContent));;
-
-//     ;
-  
-// })
 
 app.listen(PORT, () => {
     console.log("App listen port " + PORT);
